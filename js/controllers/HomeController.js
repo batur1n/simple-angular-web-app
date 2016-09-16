5748f1f0e3415ff0462089abf10c7367be91a3c0
michael <misha.baturin@gmail.com>
2016-09-16 10:03:25 +0300

    added search by select^Criteria

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,5 +1,14 @@
 app.controller('HomeController', ['$scope', 'photos', function($scope, photos) {
   photos.success(function(data) {
     $scope.photos = data;
+
+	$scope.filter = 'title';
+
+	$scope.getFilter = function() {
+    	var filter = {};
+    	filter[$scope.filter] = $scope.query;
+    	return filter;
+	};
+
   });
 }]);
