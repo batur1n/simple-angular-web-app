ef6881506bf20a4e80754687d9105b3f24aeb184
michael <misha.baturin@gmail.com>
2016-09-16 14:30:39 +0300

    improved search, added favourites dispay

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -3,12 +3,21 @@ app.controller('HomeController', ['$scope', 'photos', function($scope, photos) {
     $scope.photos = data;
 
 	$scope.filter = 'title';
-
+	
 	$scope.getFilter = function() {
     	var filter = {};
     	filter[$scope.filter] = $scope.query;
     	return filter;
 	};
+
+	$scope.plusOne = function(id) {
+		$scope.photo[id].likes += 1;
+    };
+
+    $scope.minusOne = function(id) {
+		$scope.photo[id].dislikes += 1;
+    };
+
 
   });
 }]);
--- js/controllers/PhotoController.js
+++ js/controllers/PhotoController.js
@@ -1,5 +1,13 @@
 app.controller('PhotoController', ['$scope', 'photos', '$routeParams', function($scope, photos, $routeParams) {
   photos.success(function(data) {
     $scope.detail = data[$routeParams.id];
-  });
+ });
+  
+	$scope.plusOne = function(id) {
+		$scope.detail[id].likes += 1;
+  };
+  $scope.minusOne = function(id) {
+		$scope.detail[id].dislikes += 1;
+  };
+
 }]);
