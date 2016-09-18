7a46d2b5a5caa1f7d901d670dcdd6c1a884380a8
michael <misha.baturin@gmail.com>
2016-09-18 21:21:13 +0300

    minor design fixes

--- js/app.js
+++ js/app.js
@@ -10,6 +10,10 @@ app.config(function ($routeProvider) {
     	controller: 'PhotoController',
     	templateUrl: 'views/photo.html'
   })
+	.when('/upload', {
+	  controller: 'UploadController',
+      templateURL: 'views/upload.html'
+	})
     .otherwise({ 
       redirectTo: '/' 
     }); 
